6f8f7720dae468119d1d4bc824a749addbd7220b
maxime609 <maximebisonnette13@gmail.com>
2021-01-30 15:13:34 -0500

    Partie-7-Ajout Package Building

--- packages/regression_model/regression_model/regression_model/config/config.py
+++ packages/regression_model/regression_model/regression_model/config/config.py
@@ -9,7 +9,7 @@ pd.options.display.max_rows = 10
 pd.options.display.max_columns = 10
 
 
-PACKAGE_ROOT = pathlib.Path(regression_model.__file__).resolve().parent
+PACKAGE_ROOT = pathlib.Path('C:/Users/Maxime/courA61/TP1/packages/regression_model/regression_model')
 TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
 DATASET_DIR = PACKAGE_ROOT / "datasets"
 
